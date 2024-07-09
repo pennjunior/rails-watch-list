43ab47604878b7216f6b819fe3b95d30a011f44c
Junior Penn <pennchojunior@gmail.com>
2024-07-10 01:12:38 +0900

    Show page undone

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,7 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
+    @movies = Bookmark.all
   end
 
   def show
@@ -17,7 +18,16 @@ class ListsController < ApplicationController
     redirect_to lists_path(@lists)
   end
 
+  def add_movie
+    @list = List.find(params[:id])
+    @movies = Movie.all
+  end
+
   private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
 
   def list_params
     params.require(:list).permit(:name)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_06_041205) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_06_034039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_041205) do
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "comment"
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
